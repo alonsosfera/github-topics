c319f04af2894db310787ab3d2d2136271cdfbaf
Alonso Gutierrez <alonso.gtb@gmail.com>
2022-03-29 14:17:28 -0700

    [Setup] Moves files to src directory and removes unnecessary files

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>GitHub Topics</title>
+      </Head>
+      <main>
+        <h1>Home Page</h1>
+      </main>
+    </div>
+  )
+}
