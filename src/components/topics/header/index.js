59832239bba7ae2f75e6b3d42fb43e6b2cc9ee42
Alonso Gutierrez <alonso.gtb@gmail.com>
2022-03-30 16:16:17 -0700

    [Topics] Adds TopicsHeader component

---
+++ src/components/topics/header/index.js
@@ -0,0 +1,29 @@
+import { Grid, Input } from "@nextui-org/react"
+import { debounce } from "lodash"
+
+export const TopicsHeader = ({ currentTopic, onSearch, resetTopic }) => {
+  return (
+    <>
+      <Grid xs={12} md={10}>
+        <h3 className="reset-topic">
+          Current topic: {currentTopic}
+          {currentTopic !== "react" ? (
+            <>
+              &nbsp;
+              <a onClick={resetTopic}>
+                (reset)
+              </a>
+            </>
+          ) : null}
+        </h3>
+      </Grid>
+      <Grid xs={12} md={2}>
+        <Input
+          clearable
+          aria-label="Search"
+          placeholder="Search"
+          onChange={debounce(onSearch, 800)} />
+      </Grid>
+    </>
+  )
+}
