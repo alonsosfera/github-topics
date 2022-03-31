1a831761aa5b5b7c578d02c066f3fb7b9260bf16
Alonso Gutierrez <alonso.gtb@gmail.com>
2022-03-31 00:17:47 -0700

    [Topics] Updates Topics component to handle data in parent component

--- src/components/topics/index.js
+++ src/components/topics/index.js
@@ -1,46 +1,28 @@
-import { useEffect, useState } from "react"
 import { Grid, Loading } from "@nextui-org/react"
-import { useQuery } from "@apollo/client"
-import { queries } from "../../graphql"
 import { TopicsHeader } from "./header"
 import { RelatedTopic } from "./related-topic"
 
-export const Topics = () => {
-  const [topic, setTopic] = useState("react")
-
-  const { data, loading, refetch } = useQuery(queries.topic.getTopics, {
-    variables: { name: topic }
-  })
-
-  useEffect(() => {
-    if (!topic || topic === "") {
-      refetch({ name: "react" }).then()
-    } else {
-      refetch({ name: topic }).then()
-    }
-  }, [refetch, topic])
-
-  return (
-    <Grid.Container
-      gap={1}
-      className="topics"
-      justify="center">
-      <TopicsHeader
-        currentTopic={topic}
-        resetTopic={() => setTopic("react")}
-        onSearch={({ target: { value: name } }) => setTopic(name || "react")} />
-      {loading ? (
-        <Grid>
-          <Loading type="points" />
-        </Grid>
-      ) : (
-        data.topic?.relatedTopics.map(topic =>
-          <RelatedTopic
-            data={topic}
-            key={topic.name}
-            onClick={() => setTopic(topic.name)} />
-        )
-      )}
-    </Grid.Container>
-  )
-}
+export const Topics = ({ currentTopic, data, loading, setTopic }) => (
+  <Grid.Container
+    gap={1}
+    className="topics"
+    justify="center"
+    data-testid="topics">
+    <TopicsHeader
+      currentTopic={currentTopic}
+      resetTopic={() => setTopic("react")}
+      onSearch={({ target: { value: name } }) => setTopic(name || "react")} />
+    {loading ? (
+      <Grid>
+        <Loading data-testid="loader" type="points" />
+      </Grid>
+    ) : (
+      data?.topic?.relatedTopics.map(topic =>
+        <RelatedTopic
+          data={topic}
+          key={topic.name}
+          onClick={() => setTopic(topic.name)} />
+      )
+    )}
+  </Grid.Container>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,37 @@
 import Head from "next/head"
 import { withApollo } from "../hoc"
 import { Topics } from "../components"
+import { useQuery } from "@apollo/client"
+import { queries } from "../graphql"
+import { useEffect, useState } from "react"
 
 function Home () {
+  const [topic, setTopic] = useState("react")
+
+  const { data, loading, refetch } = useQuery(queries.topic.getTopics, {
+    variables: { name: topic }
+  })
+
+  useEffect(() => {
+    if (!topic || topic === "") {
+      refetch({ name: "react" }).then()
+    } else {
+      refetch({ name: topic }).then()
+    }
+  }, [refetch, topic])
+
   return (
     <main>
       <Head>
         <title>GitHub Topics</title>
       </Head>
-      <Topics />
+      <Topics
+        data={data}
+        loading={loading}
+        setTopic={setTopic}
+        currentTopic={topic} />
     </main>
   )
 }
-
 
 export default withApollo(Home)
